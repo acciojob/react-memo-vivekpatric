85c450b5765e9d840aa34549fb2735594f89fc87
vivekpatric <patricleo223@gmail.com>
2023-08-01 10:42:08 +0530

    res

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -57,7 +57,7 @@ function MemoApp() {
           placeholder="Enter text (min. 5 characters)"
         />
         <button id="skill-btn" onClick={handleAddItem}>
-          1000000001
+          1000000000
         </button>
       </div>
     </div>
